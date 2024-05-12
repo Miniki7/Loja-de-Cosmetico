29fc747dc6551b162f02482129f0b87c04c803e1
Miniki7 <pasinimirela@gmail.com>
2024-05-11 23:53:47 -0300

    criado ShopCategory, LoginSignup e Context

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Main
+# Criando a base do nosso site de acordo com um video do youtube, o GreatStac
--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ import Product from './Pages/Product';
 import LoginSignup from './Pages/LoginSignup';
 import Shop from './Pages/Shop';
 import Cart from './Pages/Cart';
+/* importando imagens da pasta assets */
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
 
+/* men, women and kids são nossas props(propriedades) */
 
 function App() {
   return (
@@ -18,9 +23,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category="men"/>}/>
-        <Route path='/womens' element={<ShopCategory category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>}/>
         
         <Route path='/product' element={<Product/>}>
         <Route path=':productId' element={<Product/>}/>
