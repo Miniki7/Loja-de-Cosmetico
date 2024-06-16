b942cb8196fec796db124054a7882549a34c60d3
PaolaBrina <paolabrina@icloud.com>
2024-06-15 22:47:36 -0300

    cadastro funciona

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
 import { FaUser, FaLock } from "react-icons/fa"
 import './CSS/LoginSignup.css'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Link,useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 const LoginSignup = () => {
 
   const navigate = useNavigate();
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [senha, setSenha] = useState("");
+
+
+    const teste = async () => {
+        const response = await axios.get("http://localhost:3000/usuario");
+        console.log(response.data.usuario);
+        if (response.ok) {}
+    }
+
+    useEffect(() => {
+        teste()
+    }, [])
 
     const handleSubmit = async (event) => {
       event.preventDefault(); // Previne o comportamento padrão de envio de formulário
   
       try {
           // Faz uma requisição para a URL "http://localhost:3000/login" usando o método POST
-          const response = await fetch("http://localhost:3000/login", {
-              method: "POST",
-              headers: {
-                  "Content-Type": "application/json" // Define o tipo de conteúdo como JSON
-              },
-              body: JSON.stringify({ email, password }) // Converte os dados para JSON e os envia no corpo da requisição
-          });
+          const response = await axios.get("http://localhost:3000/usuario");
   
           const data = await response.json(); // Aguarda a resposta e a converte para JSON
   
@@ -29,7 +35,7 @@ const LoginSignup = () => {
           if (response.ok) {
               console.log("Login bem-sucedido", data);
               // Se o login for bem-sucedido, redireciona para "/menu_cad"
-              navigate("/menu_cad");
+              navigate("/Home");
           } else {
               console.error("Erro de login", data);
           }
@@ -45,14 +51,14 @@ const LoginSignup = () => {
       <div className="loginsignup-container">
       <form className="form-login"  onSubmit={handleSubmit}>
           <h1 className="title-login" >Entre com uma conta</h1>
-          <p className="tips"  >E-mail</p>
+          <p className="tips">E-mail</p>
           <div className="input-icon" >
               <input className="input-login" type="email" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} required />
               <FaUser className="icon-login" />
           </div>
           <p >Senha</p>
           <div >
-              <input className="input-login" type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} required />
+              <input className="input-login" type="password" placeholder="Senha" onChange={(e) => setSenha(e.target.value)} required />
               <FaLock className="icon-login" />
           </div>
           <Link className="register-login" to="/user-register">Cadastre-se</Link>
